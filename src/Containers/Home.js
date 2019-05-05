00563fe993d302d96039ec2f10ea2d7992bbe79c
MiguelMedeiros\Miguel Medeiros <miguel.medeiros@gmail.com>
2019-05-04 22:48:20 -0300

    commit initial

---
+++ src/Containers/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+// import Button from "@material-ui/core/Button";
+// import { NavLink } from "react-router-dom";
+// import ReactGA from "react-ga";
+// import {
+//   FacebookShareButton,
+//   TwitterShareButton,
+//   TelegramShareButton,
+//   WhatsappShareButton,
+//   RedditShareButton,
+//   FacebookIcon,
+//   TwitterIcon,
+//   TelegramIcon,
+//   WhatsappIcon,
+//   RedditIcon,
+// } from "react-share";
+class Home extends Component {
+  // state = {
+  //   stage: 0,
+  // };
+
+  render() {
+    return <div className="app" />;
+  }
+}
+
+export default Home;
